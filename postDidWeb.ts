09c06d8dc9919049c086c91636f4582552e041bf
Moiz Shaikh <58319530+Moiz47@users.noreply.github.com>
2025-08-26 09:06:59 +0530

    feat: generate ecdsa key pair (#16)
    Co-authored-by: moiz-sgtradex <moiz.shaikh@sgtradextech.com>

--- postDidWeb.ts
+++ postDidWeb.ts
@@ -1,26 +1,41 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { generateKeyPair, issueDID, VerificationType } from "@trustvc/w3c-issuer";
-import { Context } from "aws-lambda";
+import { CryptoSuite, generateKeyPair, issueDID, VerificationType } from "@trustvc/w3c-issuer";
 import { adjectives, animals, colors, uniqueNamesGenerator } from "unique-names-generator";
 
-export const postDidWeb = async (event: unknown, context: Context) => {
+export const postDidWeb = async (event: AWSLambda.APIGatewayEvent, context: AWSLambda.Context) => {
   try {
     const name = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals], separator: "-" });
     const domain = `${name}.${process.env.DID_DOMAIN}`;
 
+    const requestBody = event?.body
+      ? (() => {
+          try {
+            return JSON.parse(event.body);
+          } catch (err) {
+            console.error("Failed to parse request body", err);
+            return {};
+          }
+        })()
+      : {};
+
+    const { type } = requestBody;
+    const issuedDidWeb = await (type === CryptoSuite.EcdsaSd2023
+      ? issueDID({
+          domain,
+          type,
+        })
+      : (async () => {
+          const keyPair = await generateKeyPair({
+            type: VerificationType.Bls12381G2Key2020,
+          });
+          return issueDID({
+            domain,
+            ...keyPair,
+          });
+        })());
+
     const client = new DynamoDBClient({
       region: "ap-southeast-1",
-    });
-
-    // Generate a key pair
-    const keyPair = await generateKeyPair({
-      type: VerificationType.Bls12381G2Key2020,
-    });
-
-    // Generate DID document
-    const issuedDidWeb = await issueDID({
-      domain,
-      ...keyPair,
     });
 
     await client.send(
